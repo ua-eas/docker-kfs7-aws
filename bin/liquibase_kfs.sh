a90438405e85f38b3a030bfb865ba64e485b3f2b
Heather Lo <hlo@email.arizona.edu>
2020-07-07 13:57:47 -0700

    FIN-1629 Changing liquibase to 3.5.5 and extracting to new directory

--- bin/liquibase_kfs.sh
+++ bin/liquibase_kfs.sh
@@ -3,7 +3,6 @@
 # Calls liquibase with correct database parameters
 # for this environment, path to ojdbc8-19c.jar file.
 
-LIQUIBASE_HOME=$TOMCAT_SHARE_LIB
 KFS_CONFIG_FILE=$SECURITY_DIRECTORY/uaf-security-config.properties
 
 # username, password and url are passed in from the kfs config file
@@ -11,7 +10,7 @@ LIQUIBASE_DB_USERNAME=$(grep "kfs.datasource.username" $KFS_CONFIG_FILE | sed -e
 LIQUIBASE_DB_PASSWORD=$(grep "kfs.datasource.password" $KFS_CONFIG_FILE | sed -e 's/^kfs\.datasource\.password=//')
 LIQUIBASE_DB_URL=$(grep "kfs.datasource.url" $KFS_CONFIG_FILE | sed -e 's/^kfs\.datasource\.url=//')
 
-exec /usr/bin/java -jar $LIQUIBASE_HOME/liquibase-3.10.0.jar \
+exec /usr/bin/java -jar $LIQUIBASE_HOME/liquibase.jar \
 --url="$LIQUIBASE_DB_URL" \
 --username=$LIQUIBASE_DB_USERNAME \
 --password=$LIQUIBASE_DB_PASSWORD \
